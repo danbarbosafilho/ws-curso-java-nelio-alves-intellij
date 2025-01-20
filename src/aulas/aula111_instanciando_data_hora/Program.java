c1cff06f9c2948817c726a5e103e6913a96577d0
Daniel Barbosa <danielbarbosaflh@gmail.com>
2025-01-20 12:06:12 -0300

    Data-hora

--- src/aulas/aula111_instanciando_data_hora/Program.java
+++ src/aulas/aula111_instanciando_data_hora/Program.java
@@ -3,12 +3,17 @@ package aulas.aula111_instanciando_data_hora;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        // Data a partir do instante atual (agora)
+        // https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/time/format/DateTimeFormatter.html
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        // Data-hora a partir do instante atual (agora)
         LocalDate d01 = LocalDate.now(); // Local
         LocalDateTime d02 = LocalDateTime.now(); // Local
         Instant d03 = Instant.now(); // Global
@@ -17,6 +22,32 @@ public class Program {
         System.out.println("d02 = " + d02);
         System.out.println("d03 = " + d03);
 
+        // Data-hora a partir de texto ISO 8601
+        LocalDate d04 = LocalDate.parse("2025-01-20");
+        LocalDateTime d05 = LocalDateTime.parse("2025-01-21T01:37:07");
+        Instant d06 = Instant.parse("2025-01-21T01:37:07Z");
+        Instant d07 = Instant.parse("2025-01-21T01:37:07-03:00"); // -03:00 indicando horas a menos que o GMT
 
+        System.out.println();
+        System.out.println("d04 = " + d04);
+        System.out.println("d05 = " + d05);
+        System.out.println("d06 = " + d06);
+        System.out.println("d07 = " + d07);
+
+        // Data-hora a partir de texto em formato customizado
+        LocalDate d08 = LocalDate.parse("20/01/2025", fmt1);
+        LocalDateTime d09 = LocalDateTime.parse("20/01/2025 01:30", fmt2);
+
+        System.out.println();
+        System.out.println("d08 = " + d08);
+        System.out.println("d09 = " + d09);
+
+        // Data-hora a partir de dados isolados
+        LocalDate d10 = LocalDate.of(2025, 01, 20);
+        LocalDateTime d11 = LocalDateTime.of(2025, 01, 20, 12, 04);
+
+        System.out.println();
+        System.out.println("d10 = " + d10);
+        System.out.println("d11 = " + d11);
     }
 }
